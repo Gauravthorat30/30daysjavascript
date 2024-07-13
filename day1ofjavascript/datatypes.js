d392cc121ea1fa5b42577a17a43a74a3d705fa67
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-07-13 08:33:11 +0530

    Add files via upload

---
+++ day1ofjavascript/datatypes.js
@@ -0,0 +1,12 @@
+let num = 20;
+let str = "gaurav";
+let bool = true;
+let obj = {number : 40};
+obj.firstname = "thorat";
+let arr = [10, 20, 30];
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(obj);
---
+++ day1ofjavascript/variabledeclaration.js
@@ -0,0 +1,4 @@
+var x = 30;
+console.log(x);
+let y = 40;
+console.log(y);
